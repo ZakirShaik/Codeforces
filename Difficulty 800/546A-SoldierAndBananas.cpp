c2a26d84961a55550fd275c9bb01df31640f7dc2
Zakir <zaynaffanshaik@Zakir-MacBook-Pro.local>
2025-05-23 23:34:21 -0600

    Sol: 546A-Soldier and bananas

---
+++ Difficulty 800/546A-SoldierAndBananas.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int soldierAndBananas(int k, int n, int w) {
+        int totalCost = 0;
+        for(int i = 1; i <= w; ++i) totalCost += i*k;
+        return max(totalCost - n, 0);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    int k, n, w;
+    cin >> k >> n >> w;
+    Solution sol;
+    cout << sol.soldierAndBananas(k, n, w) << endl;
+    return 0;
+}
